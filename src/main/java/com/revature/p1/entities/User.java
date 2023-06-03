c037ae9acfc85c6ab0f8b78c2cd03c65f9bbc1d8
Patrick McLean <lavillelumiere8@gmail.com>
2023-06-03 19:39:13 -0400

    setup13

---
+++ src/main/java/com/revature/p1/entities/User.java
@@ -0,0 +1,20 @@
+package com.revature.p1.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private String id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "role", nullable = false)
+    private String role;
+
+}
