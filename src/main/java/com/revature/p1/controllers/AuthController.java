e39d8b06ea075039da0e8c4496dac53798804154
Patrick McLean <lavillelumiere8@gmail.com>
2023-06-06 00:51:20 -0400

    erd3

--- src/main/java/com/revature/p1/controllers/AuthController.java
+++ src/main/java/com/revature/p1/controllers/AuthController.java
@@ -24,13 +24,14 @@ public class AuthController {
     //there is also @GetMapping("/all"), @PutMapping("/update"), @DeleteMapping("/delete")
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody NewUserRequest req) {
-        //^^dto = data transfer object
+        //^^dto = data transfer object (the NewUserRequest part)
         //check for valid username
         //check for unique username
         //check for valid password
         //check password confirmation
 
         if(!userService.isUniqueUsername(req.getUsername())) {
+            //thrown exception matches handler with tag ResourceConflictException.class
             throw new ResourceConflictException("Username is not unique");
         }
 
